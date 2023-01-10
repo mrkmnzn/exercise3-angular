8baf2c5b951cf5218a701c3c7d0566cdafd96f56
mrkmnzn <mrkchrstnmnzn@gmail.com>
2023-01-10 17:18:57 +0800

    Book module routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'blog',
+    pathMatch: 'full',
+  },
+  {
+    path: 'blog',
+    loadChildren: () => import('./modules/book/book.module').then(m => m.BookModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/modules/book/book.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BookListComponent } from './pages/book-list/book-list.component';
+import { BookItemComponent } from './components/book-item/book-item.component';
+import { BookRoutingModule } from './book-routing.module';
+
+
+
+@NgModule({
+  declarations: [
+    BookListComponent,
+    BookItemComponent
+  ],
+  imports: [
+    CommonModule,
+    BookRoutingModule
+  ]
+})
+export class BookModule { }
